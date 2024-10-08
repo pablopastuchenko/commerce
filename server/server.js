049b767cc8dbb1f2e128a8712fe66736331e4cac
pastuchenko <pablopastuchenkooo@gmail.com>
2024-10-08 14:30:52 -0300

    first commit

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ mongoose
   .catch((error) => console.log(error));
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(
   cors({
@@ -41,7 +41,7 @@ app.use(
   })
 );
 
-app.get("/", (res, res) => {
+app.get("/", (req, res) => {
   res.json("Hello")
 })
 
